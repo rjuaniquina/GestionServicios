babaa74447f76a94b718d331bb7242edd44c9ad2
RJP <ramiro.juaniquina@gmail.com>
2025-01-27 23:18:36 -0400

    Ultimos Cambios

--- GestionServicios.Domain/Servicios/Servicio.cs
+++ GestionServicios.Domain/Servicios/Servicio.cs
@@ -1,6 +1,7 @@
 ﻿using GestionServicios.Domain.Abstractions;
 using GestionServicios.Domain.Shared;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,16 +13,25 @@ namespace GestionServicios.Domain.Servicios
     {
         public string Nombre { get; private set; }
         public string Descripcion { get; private set; }
-        public CostValue Costo { get; private set; }
+        public CostValue CostoUnitario { get; private set; }
         public string Estado { get; private set; }
         public Servicio(Guid id, string nombre, string descripcion, String estado) : base(id)
         {           
             Nombre = nombre;
             Descripcion = descripcion;                     
             Estado = estado;
+            CostoUnitario = 0;
         }
         public Servicio() { }
 
+        public void UpdateCost(decimal Costo)
+        {
+            if (Costo > 0)
+            {
+                CostValue newCost = Math.Round(Costo, 2);
+                CostoUnitario = newCost;               
+            }
+        }
         public Servicio(Guid id) : base(id)
         {
         }
---
+++ GestionServicios.Test/Domain/Servicios/ServicesTest.cs
@@ -0,0 +1,51 @@
+﻿using GestionServicios.Domain.Servicios;
+using GestionServicios.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionServicios.Test.Domain.Servicios
+{
+    public class ServicesTest
+    {
+        [Fact]
+        public void ServiceCreationIsValid()
+        {
+            //Arrange
+            string Nombre = "Servicio de Catering";
+            string Descripcion = "Servicio de comidas diarias entregadas en base a las recomendaciones nutricionales";
+            string Estado = "En Proceso";
+            Guid ServiceId = Guid.NewGuid();
+            //Act
+            Servicio service = new Servicio(ServiceId,Nombre,Descripcion,Estado);
+
+            // Assert
+            Assert.Equal(Nombre, service.Nombre);
+            Assert.Equal(Descripcion, service.Descripcion);
+            Assert.Equal(Estado, service.Estado);
+            Assert.Equal(0, service.CostoUnitario);          
+            Assert.Equal(ServiceId, service.Id);
+           
+        }
+
+        [Fact]
+        public void UpdateCostIsValid()
+        {
+
+            //Arrange
+            string Nombre = "Asesoramiento Nutricional";
+            string Descripcion = "Plan de alimentación de 15 días con evaluación quincenal";
+            string Estado = "En Proceso";
+            Guid ServiceId = Guid.NewGuid();
+            //Act
+            Servicio service = new Servicio(ServiceId, Nombre, Descripcion, Estado);
+            CostValue Cost = 100;
+
+            service.UpdateCost( Cost);
+
+            Assert.Equal(Cost, service.CostoUnitario);
+        }
+    }
+}
